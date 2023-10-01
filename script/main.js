d4a3de82780982aeaf67490881254fb988277705
“cain” <3111345436@qq.com>
2023-10-02 00:02:03 +0800

    配置env参数

--- script/main.js
+++ script/main.js
@@ -1,14 +1,29 @@
+/*
+ * @Author: mikey.zhaopeng
+ * @Date: 2023-10-01 23:10:32
+ * @Last Modified by: mikey.zhaopeng
+ * @Last Modified time: 2023-10-01 23:48:15
+ */
 const fs = require("fs");
 const path = require("path");
 const api = require("./api");
+require("dotenv").config();
 
 const Main = async () => {
-  const filepath = "../i18n/i18n.json";
-  const filePath = path.join(__dirname, filepath);
+  const filePath = path.join(__dirname, "../i18n/i18n.json");
   const filePathW = path.join(__dirname, "../translate/en.json");
+
   let transLateStr = "";
 
   let toJson = {};
+
+  const translateApi = {
+    baidu: api.BaiDuTranslate,
+  };
+
+  const apiType = process.env.CAIN_API_TYPE;
+  const from = process.env.CAIN_FROM;
+  const to = process.env.CAIN_TO;
 
   try {
     //  转换json文件
@@ -18,16 +33,22 @@ const Main = async () => {
     Object.keys(file).forEach((el) => {
       transLateStr += file[el] + "\n";
     });
-    const data = await api.BaiDuTranslate({
-      query: transLateStr,
-      from: "zh",
-      to: "en",
-    });
 
+    const queryData = {
+      query: transLateStr,
+      from: from,
+      to: to,
+    };
+
+    // 使用策略模式切换api方法
+    const data = await translateApi[apiType](queryData);
+
+    // 转换翻译回来的语言
     Object.keys(file).forEach((el, index) => {
       toJson[el] = data[index].dst;
     });
 
+    // 写入文件
     fs.writeFileSync(filePathW, JSON.stringify(toJson));
   } catch (err) {
     console.log("readFile Error" + err);
